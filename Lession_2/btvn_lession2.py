8a333cb61bd08fa61ff3011c75cd0d008d791945
Nguyen Tuan <anhtuantuananh98@gmail.com>
2021-04-28 16:34:38 +0300

    btvn_lession2

--- Lession_2/btvn_lession2.py
+++ Lession_2/btvn_lession2.py
@@ -24,19 +24,39 @@ Bài 02.
 a = 1998
 print(a, 'is type ', type(a))
 print(a, 'is type int? ', isinstance(a, int))
+print('+++++++++++++++++++++++++++++++++++++++++')
+a1 = 3101
+print(a1, 'is type ', type(a1))
+print(a1, 'is type bool? ', isinstance(a1, bool))
 print('------------------------------------------------------')
 b = 31.01
 print(b, ' is type ', type(b))
 print(b, ' is type int? ', isinstance(b, int))
+print('+++++++++++++++++++++++++++++++++++++++++')
+b1 = 19.98
+print(b1, 'is type ', type(b1))
+print(b1, 'is type float? ', isinstance(b1, float))
 print('------------------------------------------------------')
 c = 'I am robot'
 print(c, ' is type ', type(c))
 print(c, ' is type string? ', isinstance(c, str))
+print('+++++++++++++++++++++++++++++++++++++++++')
+c1 = 'I am student'
+print(c1, ' is type ', type(c1))
+print(c1, ' is type string? ', isinstance(c1, str))
 print('------------------------------------------------------')
 d = 10 + 3j
 print(d, ' is type ', type(d))
 print(d, ' is type complex? ', isinstance(d, complex))
+print('+++++++++++++++++++++++++++++++++++++++++')
+d1 = 40 + 0j
+print(d1, ' is type ', type(d1))
+print(d1, ' is type complex? ', isinstance(d1, complex))
 print('------------------------------------------------------')
 e = False
+print(e, ' is type ', type(e))
+print(e, ' is type bool? ', isinstance(e, bool))
+print('+++++++++++++++++++++++++++++++++++++++++')
+e = True
 print(e, ' is type ', type(e))
 print(e, ' is type bool? ', isinstance(e, bool))
